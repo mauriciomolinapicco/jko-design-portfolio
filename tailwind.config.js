0027d5da9b2458e50322fe5275c7a2decd42e626
mauriciomolinapicco <mauriscode@gmail.com>
2024-07-31 21:17:20 -0300

    personalized colors -logos page -spacing for b&w pic

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/*.{html,js}"],
+  content: ["./templates/*.{html,js}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'preto': '#222222',
+        'branco': '#F9F9F9',
+        'bege':'#C09A75',
+      }
+    },
   },
   plugins: [],
 }
-
