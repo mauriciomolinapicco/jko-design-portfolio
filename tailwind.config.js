89f4055c4ad4a0a2c31e1e90f410c0963426ccf2
mauriciomolinapicco <mauriscode@gmail.com>
2024-08-03 13:28:37 -0300

    added reveal photo on hover

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         'preto': '#222222',
         'branco': '#F9F9F9',
         'branco-2': '#FDFDFD',
-        'bege': '#C09A75'
+        'bege': '#C09A75',
+        'bege-2': '#8E6F50'
       }, 
       fontFamily: {
         body: ['Nunito sans']
