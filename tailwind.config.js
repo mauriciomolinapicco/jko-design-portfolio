bf7efaf74560e56d851f9e440ac1e9134ae1d05d
mauriciomolinapicco <mauriscode@gmail.com>
2024-08-02 21:07:06 -0300

    updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         'preto': '#222222',
         'branco': '#F9F9F9',
         'bege': '#C09A75'
+      }, 
+      fontFamily: {
+        body: ['Nunito sans']
       }
     },
   },
