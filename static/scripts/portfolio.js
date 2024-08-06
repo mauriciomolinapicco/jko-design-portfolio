265b66f0f6f0c7ae98b21086639ad8142e232959
mauriciomolinapicco <mauriscode@gmail.com>
2024-08-06 19:57:34 -0300

    new tab for portfolio, contact page bg

---
+++ static/scripts/portfolio.js
@@ -0,0 +1,10 @@
+/*document.querySelector('#externasBtn').addEventListener('click', function() {
+    document.querySelector('#internas').style.display = 'none';
+    document.querySelector('#externas').style.display = 'block';
+})
+
+document.querySelector('#internasBtn').addEventListener('click', function() {
+    document.querySelector('#externas').style.display = 'none';
+    document.querySelector('#internas').style.display = 'block';
+
+})*/
