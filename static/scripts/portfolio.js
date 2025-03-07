0a0cd16ed972eeb6fabaad0f79694624593370cd
mauriciomolinapicco <mauriscode@gmail.com>
2025-03-07 19:17:29 -0300

    reforma home page and added functionality to portfolio

--- static/scripts/portfolio.js
+++ static/scripts/portfolio.js
@@ -8,3 +8,51 @@ document.querySelector('#internasBtn').addEventListener('click', function() {
     document.querySelector('#internas').style.display = 'block';
 
 })*/
+
+const internas = document.getElementById("internas");
+const externas = document.getElementById("externas");
+const isometricas = document.getElementById("isometricas");
+
+function resetButtons() {
+    todasBtn.classList.remove("bg-bege", "text-white", "border-none", "shadow-lg");
+    internasBtn.classList.remove("bg-bege", "text-white", "border-none", "shadow-lg");
+    externasBtn.classList.remove("bg-bege", "text-white", "border-none", "shadow-lg");
+    isometricasBtn.classList.remove("bg-bege", "text-white", "border-none", "shadow-lg");
+}
+
+
+document.getElementById("todasBtn").addEventListener("click", function() {
+    resetButtons();
+    todasBtn.classList.add("bg-bege", "text-white", "border-none", "shadow-lg");
+
+    internas.style.display = "block";
+    externas.style.display = "block";
+    isometricas.style.display = "block";
+})
+
+document.getElementById("internasBtn").addEventListener("click", function() {
+    resetButtons();
+    internasBtn.classList.add("bg-bege", "text-white", "border-none", "shadow-lg");
+
+    internas.style.display = "block";
+    externas.style.display = "none";
+    isometricas.style.display = "none";
+})
+
+document.getElementById("externasBtn").addEventListener("click", function() {
+    resetButtons();
+    externasBtn.classList.add("bg-bege", "text-white", "border-none", "shadow-lg");
+
+    internas.style.display = "none";
+    externas.style.display = "block";
+    isometricas.style.display = "none";
+})
+
+document.getElementById("isometricasBtn").addEventListener("click", function() {
+        resetButtons();
+    isometricasBtn.classList.add("bg-bege", "text-white", "border-none", "shadow-lg");
+
+    internas.style.display = "none";
+    externas.style.display = "none";
+    isometricas.style.display = "block";
+})
