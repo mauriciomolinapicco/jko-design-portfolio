69a7215ddc8377c37fcd63cd0b81df4ea43e8622
Mauricio Molina <mauriscode@gmail.com>
2025-03-14 16:51:51 -0300

    connected to backend

---
+++ scripts/contact.js
@@ -0,0 +1,52 @@
+const form = document.getElementById('contact-form');
+const statusMessage = document.getElementById('statusMessage');
+
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+
+    const name = formData.get('name')
+    const email = formData.get('email')
+    const message = formData.get('message')
+
+    const data = {
+        name: name,
+        email: email,
+        message: message
+    }; 
+
+    setTimeout(() => {
+        statusMessage.textContent = 'Enviando...';
+        fetch('https://jko-backend.vercel.app/send-email', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',  // Especificamos que los datos serán en formato JSON
+            },
+            body: JSON.stringify(data)  // Convertimos el objeto data a JSON
+          })
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Error al enviar el formulario');
+              }
+              return response.json();  // Suposición de que el servidor devuelve JSON
+            })
+            .then(data => {
+              // Si todo salió bien
+              console.log('Respuesta del servidor:', data);
+              statusMessage.textContent = "¡Formulario enviado con éxito!";
+              form.reset();  // Limpiar el formulario
+            })
+            .catch(error => {
+              // Si hubo un error
+              console.error('Error:', error);
+              statusMessage.textContent = "Hubo un error al enviar el formulario. Intenta nuevamente.";
+            });
+        }, 2000)
+
+    
+
+    
+
+})
